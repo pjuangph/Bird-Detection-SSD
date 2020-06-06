79e2e2c2e9f124c6f04c9393cb54d2063121a815
Paht J <paht.juangphanich@gmail.com>
2020-06-06 00:29:05 -0400

    need to add bird images to utils

--- Object_detection/model.py
+++ Object_detection/model.py
@@ -6,6 +6,7 @@ from itertools import product as product
 import torchvision
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
 
 
 class VGGBase(nn.Module):
--- Object_detection/utils.py
+++ Object_detection/utils.py
@@ -6,6 +6,7 @@ import xml.etree.ElementTree as ET
 import torchvision.transforms.functional as FT
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
 
 # Label map
 voc_labels = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
